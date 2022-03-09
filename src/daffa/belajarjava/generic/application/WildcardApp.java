3c9bb6f13c3f8e4e2e8fbec2cdaf0bcb3cdd6dda
Daffarr <77198942+Daffarr@users.noreply.github.com>
2022-03-09 23:27:06 +0700

    1

---
+++ src/daffa/belajarjava/generic/application/WildcardApp.java
@@ -0,0 +1,13 @@
+package daffa.belajarjava.generic.application;
+
+public class WildcardApp {
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static void print(MyData<?> myData) {
+        myData
+    }
+}
